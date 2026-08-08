11679b80068bc63de3f5442d944a1a609159bb35
saepul-akbar <saepulakbar349@gmail.com>
2026-08-08 20:58:23 +0700

    content:mengubah tujuan proyek dari web profil menjadi web berita

--- Web Profile/my-app/app/page.tsx
+++ Web Profile/my-app/app/page.tsx
@@ -4,11 +4,10 @@ import InfoItem from "./components/InfoItem";
 
 export default function Home() {
   return (
-    <div>
-      <div className={styles.container}>
-        <h1 className={styles.title}>Saepul Akbar</h1>
-        InfoItem
-      </div>
+    <div className="main-heading">
+      <h1>Selamat Datang di Web AKSSI</h1>
+      <div className="subtitle">Lorem ipsum dolor, sit amet consectetur adipisicing elit. 
+        Voluptatum consectetur tempora minus blanditiis autem rerum suscipit maxime sequi beatae voluptatibus!</div>
     </div>
   );
 }
