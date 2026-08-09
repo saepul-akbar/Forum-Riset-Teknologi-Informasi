dd7d1c62b5904b39ed23be0eb60cbb858d77339a
saepul-akbar <saepulakbar349@gmail.com>
2026-08-09 10:51:43 +0700

    remove:menghapus file .next

--- Web Profile/my-app/app/components/Header.jsx
+++ Web Profile/my-app/app/components/Header.jsx
@@ -1,10 +1,11 @@
 import Link from "next/link"
+import Image from "next/image"
 
 function Header() {
   return (
     <header className="header">
         <nav>
-            <div className="logo"><Link href="/">Logo</Link></div>
+            <div className="logo"><Link href="/"><Image src="vercel.svg" width={30} height={30} alt="logo"/></Link></div>
             <div className="nav-links">
               <Link href="/">Home</Link>
               <Link href="/about">About Us</Link>
