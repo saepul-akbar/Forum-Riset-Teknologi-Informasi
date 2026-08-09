8327379d7bc3091ea968fa1d6f46ff56e5d5d176
saepul-akbar <saepulakbar349@gmail.com>
2026-08-09 10:42:50 +0700

    feat:menambahkan link Home dan About ke Nav

--- Web Profile/my-app/app/components/Header.jsx
+++ Web Profile/my-app/app/components/Header.jsx
@@ -1,10 +1,14 @@
-
+import Link from "next/link"
 
 function Header() {
   return (
     <header className="header">
         <nav>
-            <div className="logo"><a href="">Logo</a></div>
+            <div className="logo"><Link href="/">Logo</Link></div>
+            <div className="nav-links">
+              <Link href="/">Home</Link>
+              <Link href="/about">About Us</Link>
+          </div>
         </nav>
     </header>
   )
