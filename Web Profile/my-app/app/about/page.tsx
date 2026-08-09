0da00baa73f8ee60a3a25ab48b898e728c824607
saepul-akbar <saepulakbar349@gmail.com>
2026-08-09 10:30:40 +0700

    feat:menambahkan page about

---
+++ Web Profile/my-app/app/about/page.tsx
@@ -0,0 +1,11 @@
+function page() {
+  return (
+    <>
+    <h1>About Us</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae, sequi ipsum!
+      Praesentium nihil pariatur quo distinctio nulla fugit deleniti suscipit.</p>
+    </>
+  )
+}
+
+export default page
