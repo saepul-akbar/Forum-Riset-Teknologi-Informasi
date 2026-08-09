5de8df1272ecb8182e5e466b9f50bf29e4b1ed89
saepul-akbar <saepulakbar349@gmail.com>
2026-08-09 11:46:52 +0700

    feat:mengambil data dari API

--- Web Profile/my-app/app/page.tsx
+++ Web Profile/my-app/app/page.tsx
@@ -1,12 +1,36 @@
 import Image from "next/image";
-import styles from "./page.module.css"
+import Link from "next/link";
 
-export default function Home() {
+interface Post {
+    userId: number;
+    id: number;
+    title: string;
+    body: string;
+}
+
+async function getLashPost(): Promise<Post[]> {
+  const res = await fetch("https://jsonplaceholder.typicode.com/posts?_limit=5");
+  const posts: Post[] = await res.json();
+  return posts; 
+}
+
+export default async function Home() {
+  const posts = await getLashPost();
   return (
-    <div className="main-heading">
-      <h1>Selamat Datang di Web AKSSI</h1>
-      <div className="subtitle">Lorem ipsum dolor, sit amet consectetur adipisicing elit. 
-        Voluptatum consectetur tempora minus blanditiis autem rerum suscipit maxime sequi beatae voluptatibus!</div>
-    </div>
+    <>
+      <div className="main-heading">
+        <h1>Selamat Datang di Web AKSSI</h1>
+        <div className="subtitle">Lorem ipsum dolor, sit amet consectetur adipisicing elit. 
+          Voluptatum consectetur tempora minus blanditiis autem rerum suscipit maxime sequi beatae voluptatibus!</div>
+      </div>
+      <div className="post-list">
+        {posts.map((post)=>(
+            <div className="post-item" key={post.id}>
+              <h2><Link href="/">{post.title}</Link></h2>
+            </div>
+          ))
+}
+      </div>
+    </>
   );
 }
